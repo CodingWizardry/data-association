a12dfdc338319b9c7dd70c7f00f662e46e43e1fc
mudit-gohash <mudit.jain@gohashinclude.com>
2024-10-06 09:23:17 +0530

    feat: basic multer

--- app.js
+++ app.js
@@ -5,19 +5,47 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const crypto = require('crypto')
+const path = require('path')
+const multer = require('multer')
+app.use(cookieParser());
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12,  function(err, bytes){
+      const fn =  bytes.toString('hex') + path.extname(file.originalname)
+
+      cb(null, fn)
+    })
+
+  }
+})
+
+const upload = multer({ storage: storage })
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+app.post("/upload", upload.single('image'), (req, res) => {
+  console.log(req.file);
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
 app.get("/profile", isLoggedIn, async (req, res) => {
   let user = await userModel.findOne({ email: req.user.email }).populate("posts");
   res.render("profile", {user});
